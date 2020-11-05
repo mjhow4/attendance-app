691e0cf686b6e83260427e34b09116658f4ac7a2
NicholasColema <66044795+NicholasColema@users.noreply.github.com>
2020-11-05 16:53:19 -0500

    Views and Models alterations are the foucs of this push

--- courtcal/attendance/models.py
+++ courtcal/attendance/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Case(model.Model):
+     case_number = models.CharField(max_length=255, null=True, blank=True)
+     defendant_name = models.CharField(max_lenght=255,null=True, blank=True)
+     complaiant = models.CharField(max_lenght= 255,null=True, blank=True)
+     Attorney = models.CharField(max_lenght= 255,null=True, blank=True)
+     Cont = models.CharField (max_length= 255, null=True, blank=True)   
+
+
+class NoteBox(models.Model):
+    txt = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    contact = models.ForeignKey(Contact,on_delete=models.CASCADE, null=True, related_name="notes")
